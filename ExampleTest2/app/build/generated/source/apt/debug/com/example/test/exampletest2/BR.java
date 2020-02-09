f7a67b3801d4cebab4a5d89fccd058aec4d61542
Kolesnichenko <Kolesnichenko@KOLESNICHENKOOM>
2020-02-09 08:17:47 +0200

    Remove old file andfolders

--- ExampleTest2/app/build/generated/source/apt/debug/com/example/test/exampletest2/BR.java
+++
@@ -1,6 +0,0 @@
-package com.example.test.exampletest2;
-
-public class BR {
-        public static final int _all = 0;
-        public static final int editProduct = 1;
-}
--- rastergraphicspicasso/README.md
+++
@@ -1,1 +0,0 @@
-Development an application that loads a raster graphic image file from URL and applies it to ImageView using Picasso and Callback notification
